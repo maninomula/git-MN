3065755b78825132b44f5f587c72cf9f42e9e8b9
Nomula,Manikanta <s525747@ch140002m.nwmissouri.edu>
2016-11-14 13:39:40 -0600

    Added EhVC,BeeVC,SeaVC

--- Git-MN/EhVC.swift
+++ Git-MN/EhVC.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  FirstViewController.swift
 //  Git-MN
 //
 //  Created by Nomula,Manikanta on 11/14/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class EhVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
